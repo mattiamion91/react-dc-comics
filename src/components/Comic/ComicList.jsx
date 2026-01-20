a84cee9f1d860c04474a2621bc15f849ac5de57a
Mattia Mion <mattia.mion@gmail.com>
2026-01-20 16:03:34 +0100

    feat uso delle props allinternodi comiccard e assegnazione "a mano" delle proprieta da passare allinterno del tag componente

--- src/components/Comic/ComicList.jsx
+++ src/components/Comic/ComicList.jsx
@@ -3,26 +3,35 @@ import ComicCard from "./ComicCard.jsx"
 
 function ComicsList() {
 
- // funzione di rendering card fumetto
-    const renderFumettoCard = () => {
-        return comics.map((comic) => (
-            <div className="comic-card" key={comic.id}>
-                <img src={comic.thumb} alt={comic.title} />
-                <h3>{comic.series}</h3>
-            </div>
-        ))
-    }
+  // funzione di rendering card fumetto
+  const renderFumettoCard = () => {
+    return comics.map((comic) => (
+      <div className="comic-card" key={comic.id}>
+        <img src={comic.thumb} alt={comic.title} />
+        <h3>{comic.series}</h3>
+      </div>
+    ))
+  }
 
   return (
     <section id="comics">
       <div className="card-container">
         {/*{renderFumettoCard()}*/}
-        <ComicCard />
-        <ComicCard />
-        <ComicCard />
-        <ComicCard />
+        <ComicCard
+          thumb="https://imgs.search.brave.com/bf_FlGnymAgmvweY8YiLAcyy600JdSMl_jHpdR3t5ro/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvNTA5/M2IxOTEtZTkzZS00/NTJjLThhYTQtYTEw/ZDY0ZTcxN2E5LjA5/N2UwZWM5NDgzYzky/OGExMjY4Nzk1Nzk2/NTliZWNjLmpwZWc_/b2RuV2lkdGg9NjEy/Jm9kbkhlaWdodD02/MTImb2RuQmc9ZmZm/ZmZm"
+          series="Catwoman ciccio"
+          title="Catwoman Vol. 1: Copycats " />
+        <ComicCard thumb="https://imgs.search.brave.com/bf_FlGnymAgmvweY8YiLAcyy600JdSMl_jHpdR3t5ro/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvNTA5/M2IxOTEtZTkzZS00/NTJjLThhYTQtYTEw/ZDY0ZTcxN2E5LjA5/N2UwZWM5NDgzYzky/OGExMjY4Nzk1Nzk2/NTliZWNjLmpwZWc_/b2RuV2lkdGg9NjEy/Jm9kbkhlaWdodD02/MTImb2RuQmc9ZmZm/ZmZm"
+          series="Catwoman pippo"
+          title="Catwoman Vol. 1: Copycats" />
+        <ComicCard thumb="https://imgs.search.brave.com/bf_FlGnymAgmvweY8YiLAcyy600JdSMl_jHpdR3t5ro/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvNTA5/M2IxOTEtZTkzZS00/NTJjLThhYTQtYTEw/ZDY0ZTcxN2E5LjA5/N2UwZWM5NDgzYzky/OGExMjY4Nzk1Nzk2/NTliZWNjLmpwZWc_/b2RuV2lkdGg9NjEy/Jm9kbkhlaWdodD02/MTImb2RuQmc9ZmZm/ZmZm"
+          series="Catwoman luca"
+          title="Catwoman Vol. 1: Copycats" />
+        <ComicCard thumb="https://imgs.search.brave.com/bf_FlGnymAgmvweY8YiLAcyy600JdSMl_jHpdR3t5ro/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvNTA5/M2IxOTEtZTkzZS00/NTJjLThhYTQtYTEw/ZDY0ZTcxN2E5LjA5/N2UwZWM5NDgzYzky/OGExMjY4Nzk1Nzk2/NTliZWNjLmpwZWc_/b2RuV2lkdGg9NjEy/Jm9kbkhlaWdodD02/MTImb2RuQmc9ZmZm/ZmZm"
+          series="Catwoman sandro"
+          title="Catwoman Vol. 1: Copycats" />
       </div>
     </section>
-  )  
+  )
 }
 export default ComicsList
