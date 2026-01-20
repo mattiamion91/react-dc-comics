55f5c56a8b73b24252d7c83d00bd1be775088af2
Mattia Mion <mattia.mion@gmail.com>
2026-01-20 10:19:07 +0100

    feat: jumbotron

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import DCHeader from "./components/DCHeader"
 import DCMain from "./components/DCMain"
 import DCBlueComponent from "./components/DCBlueComponent"
 import DCFooter from "./components/DCFooter"
+import DCJumbotron from "./components/DCJumbotron"
 
 function App() {
-  
+
   return (
     <>
       <DCHeader />
+      <DCJumbotron />
       <DCMain />
       <DCBlueComponent />
       <DCFooter />
---
+++ src/components/DCJumbotron.jsx
@@ -0,0 +1,10 @@
+function DCJumbotron() {
+
+    return (
+        <div className="jumbotron">
+            <label htmlFor="">CURRENT SERIES</label>
+        </div>
+    )
+}
+
+export default DCJumbotron
